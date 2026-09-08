9302584f64e5e63e687dccba4373b97d0e3d588d
love <qwer1322326694@gmail.com>
2026-09-08 18:18:13 +0800

    Update clamp_speed.c

--- submissions/你的GitHub用户名/clamp_speed.c
+++ submissions/你的GitHub用户名/clamp_speed.c
@@ -2,7 +2,12 @@
 int clamp_speed(int target_speed)
 {
     /* TODO: 超出范围时返回对应边界，否则返回原值。 */
-  if(target_speed>1000)return 1000;
-  if(target_speed<-1000)return -1000;
-    return target_speed;
+if (target_speed > 1000)
+	{
+		return 1000;
+	}
+	if (target_speed < -1000)
+	{
+		return -1000;
+	}
 }
