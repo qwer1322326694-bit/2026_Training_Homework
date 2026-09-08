8255fb711bafad746de2ad7e99fd66fbeb611339
love <qwer1322326694@gmail.com>
2026-09-08 23:50:21 +0800

    交作业

---
+++ submissions/love/clamp_speed.c
@@ -0,0 +1,10 @@
+/* 作业 01：将目标速度限制在 [-1000, 1000]，补全下面的函数。 */
+int clamp_speed(int target_speed)
+{
+    /* TODO: 超出范围时返回对应边界，否则返回原值。 */
+    if (target_speed > 1000)
+        return 1000;
+    if (target_speed < -1000)
+        return -1000;
+    return target_speed;
+}
